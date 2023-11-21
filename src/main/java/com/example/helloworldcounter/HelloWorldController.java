22e1d19b318002bbe3bd9a0a254ff64781f56f56
root <root@ip-172-31-21-227.eu-north-1.compute.internal>
2023-11-21 18:30:28 +0000

    new changes added

--- src/main/java/com/example/helloworldcounter/HelloWorldController.java
+++ src/main/java/com/example/helloworldcounter/HelloWorldController.java
@@ -1,14 +1,20 @@
+package com.example.helloworldcounter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HelloWorldController {
 
     private int counter = 0;
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String hello(Model model) {
+	System.out.println("Hello, World!");
         counter++;
         model.addAttribute("message", "hello-world-" + counter);
         return "hello";
--- src/main/java/com/example/helloworldcounter/HelloWorldCounterApplication.java
+++ src/main/java/com/example/helloworldcounter/HelloWorldCounterApplication.java
@@ -1,13 +1,16 @@
 package com.example.helloworldcounter;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@ComponentScan("com.example.helloworldcounter")
 @SpringBootApplication
 public class HelloWorldCounterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldCounterApplication.class, args);
+		System.out.println("Hello, World!");
 	}
 
 }
