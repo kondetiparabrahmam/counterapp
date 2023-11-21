b37b52357ab60d04088b2dcf575aebb0ebf2237e
kondetiparabrahmam <134903475+kondetiparabrahmam@users.noreply.github.com>
2023-11-21 22:33:08 +0530

    Update HelloWorldController.java

--- src/main/java/com/example/helloworldcounter/HelloWorldController.java
+++ src/main/java/com/example/helloworldcounter/HelloWorldController.java
@@ -6,16 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloWorldController {
 
     private int counter = 0;
-/*
+
     @GetMapping("/")
     public String hello(Model model) {
         counter++;
         model.addAttribute("message", "hello-world-" + counter);
         return "hello";
-    }*/
+    }
 
-    @GetMapping("/")
+   /* @GetMapping("/")
     public String hello() {
         return "hello";
-    }
+    }*/
 }
